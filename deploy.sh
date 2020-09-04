c5248db4ad3252816223f63a9d9def6a86be3160
barefootfunk <bryant@barefootfunk.com>
2020-09-04 15:28:47 -0500

    Remove unneeded deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-yarn build
-# Todo: deploy to staging be default, special flag for production
-# Use this example: http://mywiki.wooledge.org/BashFAQ/035
-rsync -avzP --exclude 'videos/uncompressed' /Users/bryant/Sites/totf/build/ billybobjobo@billybobjobo.webfactional.com:webapps/theonetruefaith/
