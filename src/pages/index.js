e37deba030f896d2c38858c3d5d1fe06479452d4
barefootfunk <bryant@barefootfunk.com>
2021-01-13 14:16:00 -0700

    hide tix

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,81 +9,81 @@ import '../styles/App.scss';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    const eventbriteApiToken="CF3QVTIL3QGFLOPRFJKB"
+  // componentDidMount() {
+  //   const eventbriteApiToken="CF3QVTIL3QGFLOPRFJKB"
 
-    const ceremonies = [
-      {
-        number: 18,
-        theme: 'Contagious',
-        eventbriteIds: ["136060730307"]
-      }
-    ]
+  //   const ceremonies = [
+  //     {
+  //       number: 18,
+  //       theme: 'Contagious',
+  //       eventbriteIds: ["136060730307"]
+  //     }
+  //   ]
 
-    function renderTicketListings() {
-      $(`#ticket-listings`).empty();
+  //   function renderTicketListings() {
+  //     $(`#ticket-listings`).empty();
       
-      ceremonies.forEach(ceremony => {
-          $(`#ticket-listings`).append(`
-          <div id="ceremony-${ceremony.number}" class="ticket-listings__show">
-            <ul></ul>
-          </div>
-        `); 
-        ceremony.eventbriteIds.forEach(eventbriteId => {
+  //     ceremonies.forEach(ceremony => {
+  //         $(`#ticket-listings`).append(`
+  //         <div id="ceremony-${ceremony.number}" class="ticket-listings__show">
+  //           <ul></ul>
+  //         </div>
+  //       `); 
+  //       ceremony.eventbriteIds.forEach(eventbriteId => {
 
-          $.ajax({
-            "async": true,
-            "crossDomain": true,
-            "url": `https://www.eventbriteapi.com/v3/events/${eventbriteId}/?expand=ticket_classes`,
-            "method": "GET",
-            "headers": {
-              "Authorization": `Bearer ${eventbriteApiToken}`,
-              "Content-Type": "application/json"
-            }
-          }).done(function (eventData) {
+  //         $.ajax({
+  //           "async": true,
+  //           "crossDomain": true,
+  //           "url": `https://www.eventbriteapi.com/v3/events/${eventbriteId}/?expand=ticket_classes`,
+  //           "method": "GET",
+  //           "headers": {
+  //             "Authorization": `Bearer ${eventbriteApiToken}`,
+  //             "Content-Type": "application/json"
+  //           }
+  //         }).done(function (eventData) {
 
-            var startDate = new Date(eventData.start.utc);
-            const dateString = new Intl.DateTimeFormat('en', { 
-              month: 'short',
-              weekday: 'long',
-              day: '2-digit',
-            }).format(startDate);
-            const timeString = new Intl.DateTimeFormat('en', { 
-              hour: 'numeric',
-              timeZoneName: 'short', 
-            }).format(startDate);
+  //           var startDate = new Date(eventData.start.utc);
+  //           const dateString = new Intl.DateTimeFormat('en', { 
+  //             month: 'short',
+  //             weekday: 'long',
+  //             day: '2-digit',
+  //           }).format(startDate);
+  //           const timeString = new Intl.DateTimeFormat('en', { 
+  //             hour: 'numeric',
+  //             timeZoneName: 'short', 
+  //           }).format(startDate);
 
-            const tickets = eventData.ticket_classes[0];
-            const avail = tickets.quantity_total-tickets.quantity_sold;
-            const total = tickets.quantity_total;
+  //           const tickets = eventData.ticket_classes[0];
+  //           const avail = tickets.quantity_total-tickets.quantity_sold;
+  //           const total = tickets.quantity_total;
 
-            $(`#ticket-listings #ceremony-${ceremony.number} ul`).append(`
-              <li class="ticket-listings__showtime">
-                <p>
-                  ${dateString}<br>
-                  ${timeString}<br>
-                  Free to all, RSVP required
-                </p>
-                <p class="ticket-listings__showtime__availability"><span>${avail} of ${total}</span> spots remaining</p>
-                <button class="button ${avail ? '' : 'button--disabled'}" id="eventbrite-checkout-${eventbriteId}">${avail ? 'Reserve your spot' : 'TOTZ FULL UP'}</button>
-              </li>
-            `); 
+  //           $(`#ticket-listings #ceremony-${ceremony.number} ul`).append(`
+  //             <li class="ticket-listings__showtime">
+  //               <p>
+  //                 ${dateString}<br>
+  //                 ${timeString}<br>
+  //                 Free to all, RSVP required
+  //               </p>
+  //               <p class="ticket-listings__showtime__availability"><span>${avail} of ${total}</span> spots remaining</p>
+  //               <button class="button ${avail ? '' : 'button--disabled'}" id="eventbrite-checkout-${eventbriteId}">${avail ? 'Reserve your spot' : 'TOTZ FULL UP'}</button>
+  //             </li>
+  //           `); 
 
-            window.EBWidgets.createWidget({
-              widgetType: "checkout",
-              eventId: eventbriteId,
-              modal: true,
-              modalTriggerElementId: `eventbrite-checkout-${eventbriteId}`,
-              onOrderComplete: renderTicketListings
-            });
-          });
-        });
-      });
-    }
+  //           window.EBWidgets.createWidget({
+  //             widgetType: "checkout",
+  //             eventId: eventbriteId,
+  //             modal: true,
+  //             modalTriggerElementId: `eventbrite-checkout-${eventbriteId}`,
+  //             onOrderComplete: renderTicketListings
+  //           });
+  //         });
+  //       });
+  //     });
+  //   }
 
-    renderTicketListings();
+  //   // renderTicketListings();
 
-  }
+  // }
   render () {
 
     return (
